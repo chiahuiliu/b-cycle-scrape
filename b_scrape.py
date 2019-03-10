0cdb91f8ba70eac4d178a6ebc17c16fd095b6f65
ChiaHui Liu <chiahuiliu@utexas.edu>
2019-03-09 22:51:49 -0600

    update

--- b_scrape.py
+++ b_scrape.py
@@ -7,21 +7,25 @@ import pandas as pd
 import time
 import certifi
 import urllib3
+import os
 http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
-#Headers will make it look like you are using a web browser
+import datetime as dt
+import time
+
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
 
-def scrape():
-    res = []
-    for i in range (0,3376,10):
-        url = 'https://austin.bcycle.com/stations/station-locations'
-        time.sleep(3)
-        response = requests.get(url, headers=headers, verify=False).text
-        soup = BeautifulSoup(response, "lxml")
-        for s in soup.find_all("div", attrs={'class': 'markerContent markerAvailable'}):
-            res.append(s)
-    print(res[0])
-    print(len(res))
+def get_results(url, time_now):
+    response = requests.get(url, headers=headers, verify=False).text
+    soup = BeautifulSoup(response, "lxml")
+    main_text = soup.find_all("div", attrs={'id': 'MainBg2'})
+    str_main_text = str(main_text)
+    time_now = str(time_now).split('.')[0]
+    filename = "./raw_data/"+time_now+".txt"
+    print('Successfully saved'+str(filename))
+    with open(filename, "w") as f:
+        f.write(str_main_text)
 
 if __name__ == '__main__':
-    scrape()
+    while(dt.datetime.now() < dt.datetime.strptime('2019-03-25  12:00:00', '%Y-%m-%d %H:%M:%S')):
+        get_results('https://austin.bcycle.com/stations/station-locations', dt.datetime.now())
+        time.sleep(1800)
